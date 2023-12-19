c8bee96eb41b007d913aafbf0c9a81bb7130afa5
Nicolas Tudor <nicolas.tudor@keepersolutions.com>
2023-12-19 12:32:36 +0100

    removing quantisation as it is only recommended during inference

--- model.py
+++ model.py
@@ -20,15 +20,15 @@ model_name = "ybelkada/falcon-7b-sharded-bf16"
 
 # 4 bits quantisation
 
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.float16,
-)
+# bnb_config = BitsAndBytesConfig(
+#     load_in_4bit=True,
+#     bnb_4bit_quant_type="nf4",
+#     bnb_4bit_compute_dtype=torch.float16,
+# )
 
 old_model = AutoModelForCausalLM.from_pretrained(
     model_name,
-    quantization_config=bnb_config,
+    # quantization_config=bnb_config,
     # trust_remote_code=True
 )
 
